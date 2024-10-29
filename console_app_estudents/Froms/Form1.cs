fa646e74581de4db3123cb9dc509afe79ce521b5
angelina.pg <angelinapinto1278@gmail.com>
2024-10-29 05:08:48 -0600

    Se agrego metodo de agregar a form.

---
+++ console_app_estudents/Froms/Form1.cs
@@ -0,0 +1,65 @@
+﻿using console_app_estudents.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace console_app_estudents
+{
+    public partial class Form1 : Form
+    {
+        private void AddStudentToListView(studentt student)
+        {
+            // Crear un ListViewItem con los datos del estudiante
+            ListViewItem item = new ListViewItem(student.name);
+            item.SubItems.Add(student.apellido);
+            item.SubItems.Add(student.edad.ToString());
+
+            // Agregar el item al ListView
+            listView1.Items.Add(item);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Form2 newForm = new Form2();
+            this.Hide();
+            newForm.Show();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string apellido = textBox2.Text;
+            int edad = int.Parse(textBox3.Text);
+
+            studentt newStudent = new studentt(name, apellido, edad);
+
+            studentt.StudentsList.Add(newStudent);
+
+            AddStudentToListView(newStudent);
+
+            // Limpiar las cajas de texto después de agregar
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
---
+++ console_app_estudents/Models/studentt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app_estudents.Models
+{
+    class studentt
+    {
+        public static List<studentt> StudentsList = new List<studentt>();
+
+        public studentt(string name, string apellido, int edad)
+        {
+            this.name = name;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+        public string name { get; set; }
+        public string apellido { get; set; }
+        public int edad{ get; set; }
+    }
+}
